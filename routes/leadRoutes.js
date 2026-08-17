259d1650cc4fdd7bb72333ac30aec2979b67162d
gaurav <gauravkuntal393@gmail.com>
2026-08-17 17:42:52 +0530

    Fix admin middleware name mismatch in leadRoutes (admin -> adminOnly)

--- routes/leadRoutes.js
+++ routes/leadRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const { createLead, getLeads, updateLeadStatus, deleteLead } = require('../controllers/leadController')
-const { protect, admin } = require('../middleware/authMiddleware')
+const { protect, adminOnly } = require('../middleware/authMiddleware')
 
 router.post('/', createLead)
-router.get('/', protect, admin, getLeads)
-router.put('/:id', protect, admin, updateLeadStatus)
-router.delete('/:id', protect, admin, deleteLead)
+router.get('/', protect, adminOnly, getLeads)
+router.put('/:id', protect, adminOnly, updateLeadStatus)
+router.delete('/:id', protect, adminOnly, deleteLead)
 
 module.exports = router
